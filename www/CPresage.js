b269fd18fa3b6cd5224265694f85cd598fae1461
m2ok <moukliss@gmail.com>
2017-05-03 21:17:10 +0200

    CPresage bug

--- www/CPresage.js
+++ www/CPresage.js
@@ -1,14 +1,15 @@
 var CPresage = {
-	adToServe: function(onAdEvent, onAdNotFound) {
+	adToServe: function (onAdEvent, onAdNotFound) {
 		cordova.exec(onAdEvent, onAdNotFound, 'CPresage', 'adToServe', [{}]);
 	},
-    	launchWithEula: function(onAdEvent, onAdNotFound) {
+	launchWithEula: function (onAdEvent, onAdNotFound) {
 		cordova.exec(onAdEvent, onAdNotFound, 'CPresage', 'launchWithEula', [{}]);
 	},
-    	loadInterstitial: function(onAdEvent, onAdNotFound) {
+	loadInterstitial: function (onAdEvent, onAdNotFound) {
 		cordova.exec(onAdEvent, onAdNotFound, 'CPresage', 'loadInterstitial', [{}]);
 	},
-	showInterstitial: function(onAdEvent, onAdNotFound) {
+	showInterstitial: function (onAdEvent, onAdNotFound) {
 		cordova.exec(onAdEvent, onAdNotFound, 'CPresage', 'showInterstitial', [{}]);
 	}
 };
+module.exports = CPresage;
